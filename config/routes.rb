a44a8eb0490142fc7a13240fade7bb3b76658798
Jorge Borges <jorgetrabalho2016@gmail.com>
2021-11-06 14:30:42 -0300

    Adicionando rotas de manipulacao do painel de partner

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   namespace :partners do
     root to: 'home#index'
     get 'dashboard#index', to: 'dashboard#index', as: 'dashboard'
+    resources :favorites, only: [:index]
+    resources :profile, only: [:index]
+    resources :tours, only: [:index, :new, :create]
+    resources :favorites, only: [:index]
   end
   
   devise_for :users
