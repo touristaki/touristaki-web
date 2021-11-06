d502cd0edba0d6bff01d99d0722c8b9601fe6f49
Jorge Borges <jorgetrabalho2016@gmail.com>
2021-11-06 13:57:23 -0300

    Adiciona rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do  
   devise_for :partners
-  
+
   root to: "home#index"
 
   namespace :partners do
-    root 'dashboard#index'
+    root to: 'home#index'
+    get 'dashboard#index', to: 'dashboard#index', as: 'dashboard'
   end
   
   devise_for :users
